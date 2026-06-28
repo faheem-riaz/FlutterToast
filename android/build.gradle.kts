4e715ccb6506bc5f47e55f1ba0e86c1ba26d4b56
Faheem Riaz <faheem.riaz.kn@gmail.com>
2026-06-28 07:55:20 +0200

    Migrate Android build configuration to Kotlin DSL and Built-in Kotlin

---
+++ android/build.gradle.kts
@@ -0,0 +1,56 @@
+group = "io.github.ponnamkarthik.toast.fluttertoast"
+version = "1.0-SNAPSHOT"
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.13.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+    }
+}
+
+plugins {
+    id("com.android.library")
+}
+
+rootProject.allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+android {
+    compileSdk = 36
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+    }
+
+    defaultConfig {
+        minSdk = 19
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    namespace = "io.github.ponnamkarthik.toast.fluttertoast"
+
+    lint {
+        disable.add("InvalidPackage")
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.1.20")
+}
